41cf8289e3eec3016e1641a0b75d0aa8d4b826a7
teagozz <thiago2020dias@gmail.com>
2026-08-13 17:04:33 -0300

    Att: seção 5 adicionada.

---
+++ src/Secao5_If_Else/Aula41_OperadoresLogicos.java
@@ -0,0 +1,11 @@
+package Secao5_If_Else;
+
+public class Aula41_OperadoresLogicos {
+    public static void main(String[] args) {
+        // OPERADOR && = E
+        // OPERADOR || = OU
+        // OPERADOR ! = NÃO
+
+
+    }
+}
---
+++ src/Secao5_If_Else/Aula42_EstruturaCondicionalIfElse.java
@@ -0,0 +1,7 @@
+package Secao5_If_Else;
+
+public class Aula42_EstruturaCondicionalIfElse {
+    public static void main(String[] args) {
+
+    }
+}
