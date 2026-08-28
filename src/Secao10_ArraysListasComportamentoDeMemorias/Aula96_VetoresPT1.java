3e47609ac09146b578e035e651dd667327460360
teagozz <thiago2020dias@gmail.com>
2026-08-28 14:06:48 -0300

    Add: package 10 Memória, Arrays e Listas.

---
+++ src/Secao10_ArraysListasComportamentoDeMemorias/Aula96_VetoresPT1.java
@@ -0,0 +1,7 @@
+package Secao10_ArraysListasComportamentoDeMemorias;
+
+public class Aula96_VetoresPT1 {
+    // VETOR == ARRANJOS UNIDIMENSIONAIS
+    // ARRANJO(ARRAY) É UMA ESTRUTURA DE DADOS HOMOGÊNEA, ORDENADA E ALOCADA DE UMA VEZ SÓ.
+
+}
